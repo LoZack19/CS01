3020266356a7e350bfa446faf88d1effb1a25d04
Giovanni Zaccaria <giozaccaria02@gmail.com>
2025-07-28 18:07:02 +0200

    add marshaling and unmarshaling layer to solve endianess issues

--- qemu/hw/misc/s32k358_tpm.c
+++ qemu/hw/misc/s32k358_tpm.c
@@ -60,6 +60,47 @@ typedef struct {
     TPM2B_DIGEST randomBytes; // Random bytes generated
 } GetRandom_Out;
 
+/* Marshalling and unmarshalling layer */
+
+static UINT16 read_be16(Fifo8 *fifo) {
+    return ((UINT16)fifo8_pop(fifo) << 8) | (UINT16)fifo8_pop(fifo);
+}
+
+static UINT32 read_be32(Fifo8 *fifo) {
+    return ((UINT32)fifo8_pop(fifo) << 24) |
+           ((UINT32)fifo8_pop(fifo) << 16) |
+           ((UINT32)fifo8_pop(fifo) << 8)  |
+            (UINT32)fifo8_pop(fifo);
+}
+
+static void write_be16(Fifo8 *fifo, UINT16 value) {
+    fifo8_push(fifo, (value >> 8) & 0xFF);
+    fifo8_push(fifo, value & 0xFF);
+}
+
+static void write_be32(Fifo8 *fifo, UINT32 value) {
+    fifo8_push(fifo, (value >> 24) & 0xFF);
+    fifo8_push(fifo, (value >> 16) & 0xFF);
+    fifo8_push(fifo, (value >> 8) & 0xFF);
+    fifo8_push(fifo, value & 0xFF);
+}
+
+// Unmarshal a command header from the FIFO
+static void tpm_cmd_header_unmarshal(Fifo8 *fifo, tpm_cmd_header_t *header) {
+    header->tag = read_be16(fifo);
+    header->commandSize = read_be32(fifo);
+    header->commandCode = read_be32(fifo);
+}
+
+// Marshal a response header to the FIFO
+static void tpm_rsp_header_marshal(Fifo8 *fifo, const tpm_rsp_header_t *header) {
+    write_be16(fifo, header->tag);
+    write_be32(fifo, header->responseSize);
+    write_be32(fifo, header->responseCode);
+}
+
+/* Response management */
+
 static void tpm_error_response(S32k358TPMState *s, TPM_RC rc) {
     tpm_rsp_header_t rsp_header;
 
@@ -145,7 +186,7 @@ static void s32k358_tpm_process_input(S32k358TPMState *s) {
     }
 
     // Unmarshal command header from FIFO
-    fifo8_pop_buf(&s->infifo, (uint8_t *)&cmd_header, sizeof(cmd_header));
+    tpm_cmd_header_unmarshal(&s->infifo, &cmd_header);
 
     // Check if tag is valid
     if (cmd_header.tag != TPM_ST_NO_SESSIONS && cmd_header.tag != TPM_ST_SESSIONS) {
